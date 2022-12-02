adb7be88326d46de0169db1dc09a3e2c96e99e0d
Meraj <merajulhasan150@gmail.com>
2022-12-02 19:23:10 +0600

    3nd upload

--- index.js
+++ index.js
@@ -9,3 +9,17 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+ //const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yalqvm0.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   serverApi: ServerApiVersion.v1,
+// });
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0vbsoxh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri);
